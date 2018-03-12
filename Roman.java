333b424575992d42b2801f1a113ab307098e4829
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-12 17:00:32 +0530

    Update Roman.java

--- Roman.java
+++ Roman.java
@@ -1,7 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-// This function returns value of a Roman symbol
+class Roman
 int value(char r)
 {
     if (r == 'I')
@@ -21,36 +18,24 @@ int value(char r)
  
     return -1;
 }
- 
-// Returns decimal value of roman numaral
 int romanToDecimal(string &str)
 {
-    // Initialize result
     int res = 0;
- 
-    // Traverse given input
     for (int i=0; i<str.length(); i++)
     {
-        // Getting value of symbol s[i]
         int s1 = value(str[i]);
  
         if (i+1 < str.length())
         {
-            // Getting value of symbol s[i+1]
             int s2 = value(str[i+1]);
- 
-            // Comparing both values
             if (s1 >= s2)
             {
-                // Value of current symbol is greater
-                // or equal to the next symbol
                 res = res + s1;
             }
             else
             {
                 res = res + s2 - s1;
-                i++; // Value of current symbol is
-                     // less than the next symbol
+                i++; 
             }
         }
         else
@@ -61,14 +46,11 @@ int romanToDecimal(string &str)
     }
     return res;
 }
- 
-// Driver Program
-int main()
+public static void main(String arg[])
 {
-    // Considering inputs given are valid
     string str ="MCMIV";
-    cout << "Integer form of Roman Numeral is "
-         << romanToDecimal(str) << endl;
+    System.out.println( "Integer form of Roman Numeral is "
+        + romanToDecimal(str) );
  
     return 0;
 }
