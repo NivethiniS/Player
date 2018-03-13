90a2ef6f0eea56ee68b32117624fb5c0aef82e50
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-13 17:20:38 +0530

    Create Shift.java

---
+++ Shift.java
@@ -0,0 +1,14 @@
+public class Shift
+{
+
+	public static void main(String[] args) {
+		 
+				int a=2;
+				int i;
+				i=a<<1;
+				System.out.println("the value of a before left shift is: " +a);
+				System.out.println("the value of a after applying left shift is: " +i);
+		
+	}
+
+}
