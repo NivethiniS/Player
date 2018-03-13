c3f275b08909bf98951d3a4e9f9cd4736054bf8a
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-13 17:23:24 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,11 @@
+public class Square
+{
+    public static void main(String[] args)
+    {
+        double number = 16;
+        double squareRoot = Math.sqrt(number);
+        System.out.println("number : "+number);
+        System.out.println("Square Root : "+squareRoot);
+    }
+ 
+}
